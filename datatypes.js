1679b21be915420866a86137d626c8a4e51130d5
sanishmdhr96 <mail.sanishmanandhar@gmail.com>
2024-05-27 18:46:46 +0545

    data types

---
+++ datatypes.js
@@ -0,0 +1,43 @@
+// primitive data types
+let fruit = 'apple'
+let result = 'fail'
+let number = 2
+let boolean = true / false
+
+let name = undefined;
+
+
+// non - primitive data types
+let object = {
+    name: "sanish",
+    number: 1111,
+    isTrue: true,
+}
+
+//string => group of characters sanish
+/**
+ * "dsad ", 'dasd ', `  `
+ *
+ * 
+ * Number => numeric value , integer, floating points
+ *  
+ * Boolean => true, false, 1/0
+ * 
+ * undefined => declared for no value
+ * 
+ *  
+ */
+
+let subject = {
+    name: 'physics',
+    marks: 100
+};
+
+subject.name;
+subject.marks
+
+let colors = null;
+
+console.log('subject', subject)
+console.log('colors', colors)
+
